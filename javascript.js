af436f5d08879542bcac011cec5b3236e87099d5
wions37 <gpdnjs0307@gmail.com>
2024-05-27 20:36:49 +0900

    frontproject5

--- javascript.js
+++ javascript.js
@@ -1,5 +1,4 @@
-
-
+//
 
 const mapContainer = document.querySelector('.mapp'), // 지도를 표시할 div
     mapOption = {
@@ -7,5 +6,48 @@ const mapContainer = document.querySelector('.mapp'), // 지도를 표시할 div
         level: 3 // 지도의 확대 레벨
     };
 
-// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
-const map = new kakao.maps.Map(mapContainer, mapOption);
+const map = new kakao.maps.Map(mapContainer, mapOption); // 지도를 생성합니다
+
+// 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+const mapTypeControl = new kakao.maps.MapTypeControl();
+
+// 지도에 컨트롤을 추가해야 지도위에 표시됩니다
+// kakao.maps.ControlPosition은 컨트롤이 표시될 위치를 정의하는데 TOPRIGHT는 오른쪽 위를 의미합니다
+map.addControl(mapTypeControl, kakao.maps.ControlPosition.TOPRIGHT);
+
+// 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+const zoomControl = new kakao.maps.ZoomControl();
+map.addControl(zoomControl, kakao.maps.ControlPosition.RIGHT);
+
+
+// /-----------------------------------------------/
+const listPic = document.querySelector('.morbtn');
+const btn = document.querySelector('button');
+let pageToPatch = 1;
+
+
+btn.addEventListener('click', ()=>{fetchImages(pageToPatch += 1)});
+
+async function fetchImages(page){
+    try{
+        const response = await fetch(`https://picsum.photos/v2/list?page=${page}&limit=5`);
+
+        if(!response.ok){
+            throw new Error('네트워크 응답에 문제가 있습니다.');
+        }
+
+        // 제이슨 데이터를 자바스크립트 객체로 파싱
+        const datas = await response.json();
+        console.log(datas);
+        makeImageList(datas);
+
+    }catch(error){
+        console.error(error);
+    }
+}
+
+function makeImageList(datas){
+    datas.forEach((data)=>{
+        listPic.insertAdjacentHTML('beforeend', `<li class="img-width"><img src="${data.download_url}" alt=""></li>`);
+    });
+}
